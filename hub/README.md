4bcbb08d33adc57e2abd495d4cd5937db3228184
Ascension Association <fltr@ascension.us.org>
2024-08-24 14:57:10 -0700

    headscale instructions

--- hub/README.md
+++ hub/README.md
@@ -1,3 +1,20 @@
 # FLTR Hub
 
-Centralized data handling
+Centralized instance for managing the FLTR nodes, aggregating logs, and communicating with the FLTR app.
+
+### Prerequisites
+
+1. Purchase a domain name
+2. On a secure device, generate your own Ed25519 SSH key pair: `ssh-keygen -t ed25519`
+3. Create an Alpine Linux compute instance with a static public IPv4 address
+4. If you're running within a container or virtual cloud network, expose or add ingress rules for ports 22, 80, and 443
+5. Create a DNS A record pointing your static public IPv4 address to your domain or a new subdomain (may take up to a day to propagate)
+
+### Installation
+
+1. SSH into the Alpine Linux instance as root
+2. Install bash (`apk add bash`)
+3. Download the `fltr-hub-setup.bash` and `fltr-hub-setup.conf.template` files
+4. Rename the `fltr-hub-setup.conf.template` file to `fltr-hub-setup.conf` and edit with your values
+5. Run `bash fltr-hub-setup.bash`
+6. Verify Headscale is running by browsing to https://YOUR-DOMAIN-HERE/windows
---
+++ hub/fltr-hub-setup.bash
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+# load configuration settings
+while read LINE; do declare "$LINE"; done <fltr-hub-setup.conf
+
+# verify root
+if [ "${EUID}" -ne 0 ]; then
+  echo "Please run as root"
+  exit
+fi
+
+# verify SSH key
+if [[ -z "${ED25519_PUB_KEY}" ]] || [[ "${ED25519_PUB_KEY}" != "ssh-ed25519 "* ]]; then
+  echo 'Missing Ed25519 public key. Create one via `ssh-keygen -t ed25519` and enter the public key in fltr-hub-setup.conf'
+  exit
+fi
+
+# verify domain name
+if [[ -z "${DOMAIN_NAME}" ]] || [[ "${DOMAIN_NAME}" != *"."* ]]; then
+  echo 'Missing domain name. Please see README for instructions.'
+  exit
+fi
+
+# verify email
+if [[ -z "${ACME_EMAIL}" ]] || [[ "${ACME_EMAIL}" != *"@"* ]]; then
+  echo "Missing email address, which is required by Let's Encrypt. Please see README for instructions."
+  exit
+fi
+
+# use SafeSurfer.io upstream DNS servers
+sed -i "/^nameserver.*$/d" /etc/resolv.conf
+echo "nameserver 104.197.28.121" >>/etc/resolv.conf
+echo "nameserver 104.155.237.225" >>/etc/resolv.conf
+
+# enable community repo and use TLS
+sed -i "s/#//" /etc/apk/repositories
+sed -i "s/^http:/https:/g" /etc/apk/repositories
+apk update
+
+# install tinyssh
+if [ "$(apk list --installed | grep tinyssh | wc -l)" -eq 0 ]; then
+  echo "Installing tinyssh..."
+  apk del openssh dropbear
+  apk add tinyssh
+  rc-update add tinysshd
+  mkdir -p ~/.ssh
+  echo "${ED25519_PUB_KEY}" >~/.ssh/authorized_keys
+  chmod 700 ~/.ssh
+  chmod 600 ~/.ssh/authorized_keys
+  rc-service tinysshd start
+fi
+
+# disable IPv6
+echo 1 >/proc/sys/net/ipv6/conf/all/disable_ipv6
+
+# install endwall
+if [ ! -f /usr/local/bin/endwall ]; then
+  echo "Installing endwall..."
+  apk del iptables ip6tables
+  apk add curl nftables iproute2 nmap
+  rc-update add nftables
+  rc-service nftables start
+  curl -sLo /usr/local/bin/endwall https://raw.githubusercontent.com/ascension-association/endwall/master/endwall_nft_alpine.sh
+  sed -i "s/#server_in tcp 22/server_in tcp 22/" /usr/local/bin/endwall
+  sed -i "s/#server_in_x tcp 80,443/server_in_x tcp 80,443/" /usr/local/bin/endwall
+  sed -i "s/#server_in_x tcp 80,443/server_in_x tcp 80,443/" /usr/local/bin/endwall
+  chmod u+wrx /usr/local/bin/endwall
+  /usr/local/bin/endwall
+fi
+
+# install Headscale
+apk add headscale --repository=https://dl-cdn.alpinelinux.org/alpine/edge/testing
+echo '{"acls":[{"action":"accept","src":["*"],"dst":["*:*"]}]}' >/etc/headscale/acl.hujson
+chmod 644 /etc/headscale/acl.hujson
+sed -i "s/^server_url.*$/server_url: https:\/\/${DOMAIN_NAME}:443/" /etc/headscale/config.yaml
+sed -i "s/^listen_addr.*$/listen_addr: 0.0.0.0:443/" /etc/headscale/config.yaml
+sed -i "s/^acme_email.*$/acme_email: \"${ACME_EMAIL}\"/" /etc/headscale/config.yaml
+sed -i "s/^tls_letsencrypt_hostname.*$/tls_letsencrypt_hostname: \"${DOMAIN_NAME}\"/" /etc/headscale/config.yaml
+sed -i "s/^acl_policy_path.*$/acl_policy_path: \"\/etc\/headscale\/acl.hujson\"/" /etc/headscale/config.yaml
+sed -i "s/base_domain: example\.com/base_domain: ${DOMAIN_NAME}/" /etc/headscale/config.yaml
+sed -i "s/magic_dns: true/magic_dns: false/" /etc/headscale/config.yaml
+sed -i "s/override_local_dns: false/override_local_dns: true/" /etc/headscale/config.yaml
+sed -i "s/^    - 1\.1\.1\.1.*$/    - 104.197.28.121/" /etc/headscale/config.yaml
+sed -i "/104\.197\.28\.121/a \    \- 104.155.237.225" /etc/headscale/config.yaml
+# need to run as root due to privileged 443 port
+sed -i "s/^command_user=.*/command_user=\"root:root\"/" /etc/init.d/headscale
+rc-update add headscale
+rc-service headscale start
+
+exit
